fcd3deb9472584e0488a6259f8801a4b5ac888fb
wesleyruchel <wesleyruchel@gmail.com>
2024-09-01 03:32:34 -0300

    Routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
-import { route } from 'quasar/wrappers'
-import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
-import routes from './routes'
+import { route } from "quasar/wrappers";
+import {
+  createRouter,
+  createMemoryHistory,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
+import routes from "./routes";
+import { isAuthenticated, refreshAcessToken } from "src/api/authService";
 
 /*
  * If not building with SSR mode, you can
@@ -14,7 +20,9 @@ import routes from './routes'
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
+    : process.env.VUE_ROUTER_MODE === "history"
+    ? createWebHistory
+    : createWebHashHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -23,8 +31,20 @@ export default route(function (/* { store, ssrContext } */) {
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.VUE_ROUTER_BASE)
-  })
+    history: createHistory(process.env.VUE_ROUTER_BASE),
+  });
 
-  return Router
-})
+  Router.beforeEach(async (to, from, next) => {
+    if (to.meta.requiresAuth) {
+      if (!isAuthenticated()) {
+        const tokenRefreshed = await refreshAcessToken();
+        if (!tokenRefreshed) {
+          return next("/sign-in");
+        }
+      }
+    }
+    next();
+  });
+
+  return Router;
+});
