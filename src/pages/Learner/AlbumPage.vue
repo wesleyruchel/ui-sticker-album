ef9d5c5ba05e1f627d9a2c530646063d696ae7db
wesleyruchel <wesleyruchel@gmail.com>
2024-09-12 22:48:03 -0300

    Creating pages for a learner.

---
+++ src/pages/Learner/AlbumPage.vue
@@ -0,0 +1,287 @@
+<template>
+  <q-page class="q-pa-lg">
+    <div class="row items-center justify-between q-mt-md q-mb-md q-mx-md">
+      <div class="text-h5">{{ albumName }}</div>
+      <div>
+        <q-btn rounded color="primary" label="Início" @click="goToHome" />
+      </div>
+    </div>
+    <div class="row q-gutter-md section-card" flat bordered>
+      <q-card
+        v-for="sticker in albumStickers"
+        :key="sticker.id"
+        class="col-12 col-md-2 rounded-card"
+        flat
+        bordered
+      >
+        <q-card-section>
+          <div class="text-h5 q-mt-sm q-mb-xs">{{ sticker.title }}</div>
+          <div class="text-caption">
+            {{ truncatedDescription(sticker.description) }}
+            <span v-if="isDescriptionTruncated(sticker.description)">...</span>
+            <q-btn
+              v-if="isDescriptionTruncated(sticker.description)"
+              flat
+              dense
+              color="primary"
+              class="text-caption"
+              label="Ver mais"
+              @click="
+                openFullDescriptionStikcerDialog(
+                  sticker.description,
+                  sticker.title
+                )
+              "
+            />
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <q-img
+            :src="
+              userStickersImages[sticker.id]
+                ? userStickersImages[sticker.id]
+                : 'https://cdn.quasar.dev/img/parallax2.jpg'
+            "
+          >
+            <template v-slot:error>
+              <div class="q-pa-md flex flex-center full-width full-height">
+                <q-icon name="camera" color="grey-4" />
+              </div>
+            </template>
+          </q-img>
+        </q-card-section>
+        <q-card-section>
+          <div class="items-center justify-between q-mt-md q-mb-md">
+            <q-btn
+              rounded
+              color="primary"
+              label="Carregar"
+              icon="upload"
+              @click="uploadImage(sticker.id)"
+            />
+            <q-btn
+              rounded
+              color="primary"
+              label="Capturar"
+              icon="camera_alt"
+              @click="captureImage(sticker.id)"
+            />
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+
+  <q-dialog v-model="fullDescriptionStikcerDialog" persistent>
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">{{ fullDescriptionAboutStickerTitle }}</div>
+      </q-card-section>
+      <q-card-section>
+        <div>{{ fullDescriptionAboutSticker }}</div>
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn flat label="Fechar" color="primary" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+
+  <CameraModal
+    :isOpen="cameraModalOpen"
+    @photo-captured="handlePhotoCaptured"
+  />
+
+  <input
+    type="file"
+    ref="fileInput"
+    @change="onFileChange"
+    accept="image/*"
+    style="display: none"
+  />
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { fetchStickersAlbumById } from "src/api/albumsService";
+import { getPostedSticker, postSticker } from "src/api/userService";
+import { getDynamicRoute } from "src/utils/routeHelpers";
+import { HandlerLoading } from "src/utils/loaderUtils";
+import useNotifications from "src/utils/notificationUtils";
+import CameraModal from "src/components/CameraModal.vue";
+
+const route = useRoute();
+const router = useRouter();
+const { showSuccessNotification, showErrorNotification } = useNotifications();
+
+const albumId = ref(route.params.id || null);
+const albumName = ref(route.params.name || "");
+const albumStickers = ref({});
+const handlerSticker = ref({ id: 0 });
+const userStickers = ref({});
+const userStickersImages = ref({});
+
+onMounted(() => {
+  fetchAlbumStickersData(albumId.value);
+});
+
+const goToHome = () => {
+  router.push(getDynamicRoute("inicio"));
+};
+
+const fetchAlbumStickersData = async (albumId) => {
+  try {
+    HandlerLoading.show("Aguarde... Carregando...");
+    const { stickers } = await fetchStickersAlbumById(albumId);
+
+    if (stickers) {
+      albumStickers.value = stickers;
+      const postedStickers = await getPostedSticker(albumId);
+
+      if (postedStickers) {
+        userStickers.value = postedStickers;
+        albumStickers.value.forEach((s) => {
+          const data = postedStickers.data.find(
+            (image) => image.stickerId === s.id
+          );
+
+          if (data) userStickersImages.value[s.id] = data.imageUrl;
+        });
+      }
+    }
+  } catch (error) {
+    showErrorNotification(error.message);
+  } finally {
+    HandlerLoading.hide();
+  }
+};
+
+const fullDescriptionStikcerDialog = ref(false);
+const fullDescriptionAboutSticker = ref("");
+const fullDescriptionAboutStickerTitle = ref("");
+const maxCharsForDescriptionAboutSticker = 30;
+
+const isDescriptionTruncated = (description) =>
+  description.length > maxCharsForDescriptionAboutSticker;
+
+const truncatedDescription = (description) =>
+  isDescriptionTruncated(description)
+    ? description.substring(0, maxCharsForDescriptionAboutSticker)
+    : description;
+
+const openFullDescriptionStikcerDialog = (description, title) => {
+  fullDescriptionAboutSticker.value = description;
+  fullDescriptionAboutStickerTitle.value = title;
+  fullDescriptionStikcerDialog.value = true;
+};
+
+const fileInput = ref(null);
+
+const uploadImage = async (stickerId) => {
+  handlerSticker.value.id = stickerId;
+  fileInput.value.click();
+};
+
+const onFileChange = (event) => {
+  try {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        const imageBase64 = new String(e.target.result).split(",")[1];
+
+        try {
+          if (handlerSticker.value.id > 0 && imageBase64.length > 0) {
+            HandlerLoading.show(
+              "Aguarde... estamos colando a figurinha para você..."
+            );
+
+            const posted = await postSticker({
+              imageBase64,
+              stickerId: handlerSticker.value.id,
+            });
+
+            if (posted) {
+              userStickersImages.value[handlerSticker.value.id] =
+                e.target.result;
+              showSuccessNotification("Uhul! Nova figurinha adicionada!");
+            }
+          }
+        } catch (error) {
+          showErrorNotification(error.message);
+        } finally {
+          HandlerLoading.hide();
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  } catch (error) {
+    showErrorNotification(error.message);
+  } finally {
+  }
+};
+
+const cameraModalOpen = ref(false);
+const capturedImage = ref(null);
+
+const captureImage = async (stickerId) => {
+  cameraModalOpen.value = true;
+  handlerSticker.value.id = stickerId;
+};
+
+const handlePhotoCaptured = async (photo) => {
+  capturedImage.value = photo;
+  cameraModalOpen.value = false;
+
+  try {
+    const imageBase64 = new String(capturedImage.value).split(",")[1];
+
+    if (handlerSticker.value.id > 0 && imageBase64.length > 0) {
+      HandlerLoading.show(
+        "Aguarde... estamos colando a figurinha para você..."
+      );
+
+      const posted = await postSticker({
+        imageBase64,
+        stickerId: handlerSticker.value.id,
+      });
+
+      if (posted) {
+        userStickersImages.value[handlerSticker.value.id] = photo;
+        showSuccessNotification("Uhul! Nova figurinha adicionada!");
+      }
+    }
+  } catch (error) {
+    showErrorNotification(error.message);
+  } finally {
+    HandlerLoading.hide();
+  }
+};
+</script>
+
+<style scoped>
+.text-h5 {
+  font-weight: bold;
+}
+
+.rounded-card {
+  width: 100%;
+  max-width: 340px;
+  border-radius: 12px;
+  transition: transform 0.2s;
+}
+
+.rounded-card:hover {
+  transform: scale(1.02);
+}
+
+.q-mx-md {
+  margin-left: 30px;
+  margin-right: 30px;
+}
+
+.section-card {
+  margin-left: 20px;
+  margin-right: 20px;
+}
+</style>
---
+++ src/pages/Learner/HomePage.vue
@@ -0,0 +1,131 @@
+<template>
+  <q-page class="q-pa-lg">
+    <div class="row items-center justify-between q-mt-md q-mb-md q-mx-md">
+      <div class="text-h5">Meus álbums</div>
+      <div>
+        <q-btn
+          unelevated
+          rounded
+          label="Novo"
+          color="primary"
+          class="q-ml-sm"
+          @click="prompt = true"
+        />
+        <q-dialog v-model="prompt" persistent>
+          <q-card style="min-width: 350px">
+            <q-card-section>
+              <div class="text-h6">Código do álbum compartilhado</div>
+            </q-card-section>
+            <q-card-section class="q-pt-none">
+              <q-input
+                dense
+                v-model="sharedCode"
+                autofocus
+                @keyup.enter="prompt = false"
+              />
+            </q-card-section>
+            <q-card-actions align="right" class="text-primary">
+              <q-btn flat label="Cancelar" v-close-popup />
+              <q-btn
+                flat
+                label="Adicionar álbum!"
+                v-close-popup
+                @click="getAlbum"
+              />
+            </q-card-actions>
+          </q-card>
+        </q-dialog>
+      </div>
+    </div>
+    <div class="row q-gutter-md section-card" flat bordered>
+      <q-card
+        v-for="album in albums"
+        :key="album.id"
+        class="col-12 col-md-2 rounded-card"
+        flat
+        bordered
+        @click="goToAlbum(album.id, album.title)"
+      >
+        <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" />
+        <q-card-section>
+          <div class="text-h5 q-mt-sm q-mb-xs">{{ album.title }}</div>
+          <div class="text-subtitle1 q-mb-sm">{{ album.description }}</div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { fetchAlbums, getSharedAlbum } from "src/api/userService";
+import { HandlerLoading } from "src/utils/loaderUtils";
+import useNotifications from "src/utils/notificationUtils";
+
+const router = useRouter();
+const { showErrorNotification } = useNotifications();
+
+const albums = ref({});
+const prompt = ref(false);
+const sharedCode = ref("");
+
+onMounted(() => {
+  fetchAlbumsData();
+});
+
+const fetchAlbumsData = async () => {
+  try {
+    HandlerLoading.show("Aguarde... Estamos carregando seus álbuns...");
+    const { items } = await fetchAlbums();
+    albums.value = items;
+  } catch (error) {
+    showErrorNotification(error.message);
+  } finally {
+    HandlerLoading.hide();
+  }
+};
+
+const getAlbum = async () => {
+  try {
+    const response = await getSharedAlbum(sharedCode.value);
+    if (response) {
+      router.go();
+    }
+  } catch (error) {
+    showErrorNotification(error.message);
+  }
+};
+
+const goToAlbum = async (albumId, albumTitle) => {
+  router.push(`album/${albumId}/${albumTitle}`);
+};
+</script>
+
+<style scoped>
+.text-h5 {
+  font-weight: bold;
+}
+
+.rounded-card {
+  width: 100%;
+  max-width: 340px;
+  border-radius: 12px;
+  cursor: pointer;
+  transition: transform 0.2s;
+}
+
+.rounded-card:hover {
+  transform: scale(1.02);
+}
+
+.q-mx-md {
+  margin-left: 30px;
+  margin-right: 30px;
+}
+
+.section-card {
+  margin-left: 20px;
+  margin-right: 20px;
+}
+</style>
