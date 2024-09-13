f06253a842536a79923a1894d670185ec1f39f94
wesleyruchel <wesleyruchel@gmail.com>
2024-09-12 22:11:10 -0300

    Checking if error object has response property.

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -28,7 +28,11 @@ api.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
       originalRequest._retry = true;
       const authStore = useAuthStore();
       await authStore.refreshAccessToken();
