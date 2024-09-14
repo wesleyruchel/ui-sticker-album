e075622ae051bd37ecf094864aafd30ad84840da
wesleyruchel <wesleyruchel@gmail.com>
2024-09-14 17:08:02 -0300

    Creating the sqlServerDateTimeFormatToDDMMYYYY function.

--- src/utils/common.js
+++ src/utils/common.js
@@ -4,6 +4,14 @@ export function sqlServerDateTimeFormat(dateString) {
   return isoString.split("T")[0] + "T00:00:00";
 }
 
+export function sqlServerDateTimeFormatToDDMMYYYY(dateString) {
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+}
+
 function parseDate(input) {
   const [day, month, year] = input.split("/");
   return new Date(`${year}-${month}-${day}`);
