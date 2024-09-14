3bc5ded90a8d5ed3585b15746fba38ab18c1dc33
wesleyruchel <wesleyruchel@gmail.com>
2024-09-14 17:07:19 -0300

    Improving UserProfilePage.

--- src/pages/UserProfilePage.vue
+++ src/pages/UserProfilePage.vue
@@ -7,21 +7,32 @@
       <q-card-section>
         <q-form @submit.prevent="handleSubmit">
           <q-input
-            v-model="formData.name"
+            v-model="userData.firstName"
             label="Nome"
             rounded
             outlined
             class="q-mb-md"
+            lazy-rules
+            :rules="nameRules"
           />
           <q-input
-            v-model="formData.email"
-            label="E-mail"
+            v-model="userData.lastName"
+            label="Sobrenome"
             rounded
             outlined
             class="q-mb-md"
           />
           <q-input
-            v-model="formData.bornDate"
+            v-model="userData.email"
+            label="E-mail"
+            rounded
+            outlined
+            class="q-mb-md"
+            lazy-rules
+            :rules="emailRules"
+          />
+          <q-input
+            v-model="userData.bornDate"
             label="Data nascimento"
             rounded
             outlined
@@ -31,17 +42,26 @@
             <template v-slot:append>
               <q-icon name="event" class="cursor-pointer">
                 <q-popup-proxy>
-                  <q-date v-model="formData.bornDate" mask="DD/MM/YYYY" />
+                  <q-date v-model="userData.bornDate" mask="DD/MM/YYYY" />
                 </q-popup-proxy>
               </q-icon>
             </template>
           </q-input>
+
           <div class="row justify-end q-pb-md">
             <q-btn label="Alterar dados" type="submit" color="primary" />
           </div>
         </q-form>
         <q-form @submit.prevent="handleSubmitChangePassword">
-          <PasswordConfirmation v-model="formData" />
+          <q-input
+            v-model="userData.username"
+            label="Nome de usuário"
+            rounded
+            outlined
+            class="q-mb-md"
+            :disable="true"
+          />
+          <PasswordConfirmation v-model="userData" />
           <div class="row justify-end">
             <q-btn label="Alterar senha" type="submit" color="primary" />
           </div>
@@ -52,31 +72,58 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
+import { fetchProfile } from "src/api/userService";
 import PasswordConfirmation from "src/components/PasswordConfirmation.vue";
 import useNotifications from "src/utils/notificationUtils";
 
 const router = useRouter();
 const { showSuccessNotification, showErrorNotification } = useNotifications();
 
-const formData = ref({
-  name: "NÃO IMPLEMENTADO",
-  email: "NAOIMPLEMENTADO@EXEMPLO.COM",
-  bornDate: "",
+const userData = ref({
+  type: "",
+  firstName: "",
+  lastName: "",
+  email: "",
+  bornDate: null,
+  username: "",
   password: "",
   confirmPassword: "",
+});
+
+const nameRules = [
+  (val) => (val && val.length > 0) || "É necessário informar um nome.",
+];
+
+const emailRules = [
+  (val) => (val && val.length > 0) || "É necessário informar um e-mail.",
+  (val) =>
+    /^\S+@\S+\.\S+$/.test(val) || "É necessário informar um e-mail válido.",
+];
+
+onMounted(async () => {
+  fetchUserData();
 });
 
 const goToBack = () => {
   router.back();
 };
 
+const fetchUserData = async () => {
+  const response = await fetchProfile();
+  userData.value = response.data;
+};
+
 const handleSubmit = async () => {
-  showErrorNotification("Oops...");
+  showErrorNotification(
+    "Desculpe, ainda não implementamos essa funcionalidade :("
+  );
 };
 
 const handleSubmitChangePassword = async () => {
-  showErrorNotification("Oops...");
+  showErrorNotification(
+    "Desculpe, ainda não implementamos essa funcionalidade :("
+  );
 };
 </script>
